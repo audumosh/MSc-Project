b70d7c21de3026c66275f51bebc15dad880b2e3d
moshood.au@gmail.com <moshood.au@gmail.com>
2024-06-11 09:49:19 -0300

    Research question 1, preliminary analysis

--- MSc Project.R
+++ MSc Project.R
@@ -9,11 +9,6 @@ library(tidyverse)
 ## Read excel file
 PAHO_COVID_Projects <- read.xlsx(xlsxFile = "COVID-19-Research-Project-Tracker-PAHO-foR.xlsx")
 
-# Function to count unique countries per funder
-count_unique_countries <- function(...) {
-  unique_countries <- unique(unlist(list(...)))
-  return(length(unique_countries))
-}
 
 
 # Check the column names to ensure correct reference
@@ -24,23 +19,20 @@ PAHO_COVID_Projects <- PAHO_COVID_Projects %>%
   mutate(Amount.Awarded = as.numeric(gsub("[^0-9.]", "", Amount.Awarded)))
 
 
-# Descriptive analysis
-# Data processing
-result <- PAHO_COVID_Projects %>%
+### Research question 1: What were the primary sources of funding for COVID-19 research conducted in the PAHO region?
+
+## Descriptive and statiscal analysis of COVID-19 research funding sources
+
+# Number of research projects funded, number of countries where funded projects are being conducted and total amount mapped to funders
+
+result_1 <- PAHO_COVID_Projects %>%
+  # Split the 'Country/ countries research are being conducted' into multiple rows
+  separate_rows(`Country/.countries.research.are.being.conducted`, sep = ",") %>%
+  mutate(`Country/.countries.research.are.being.conducted` = trimws(`Country/.countries.research.are.being.conducted`)) %>%
   group_by(Funders) %>%
   summarise(
     Total_Projects = n(),
-    Total_Amount_Awarded = sum(Amount.Awarded, na.rm = TRUE)
+    No_of_Countries = n_distinct(`Country/.countries.research.are.being.conducted`),
+    Total_Amount_Awarded = sum(Amount.Awarded, na.rm = TRUE),
     )
 
-
-
-# Split the 'Country/ countries research is being conducted' into multiple rows
-PAHO_COVID_Projects <- PAHO_COVID_Projects %>%
-  separate_rows(`Country/.countries.research.is.being.are.conducted`, sep = ",") %>%
-  mutate(`Country/.countries.research.is.being.are.conducted` = trimws(`Country/.countries.research.is.being.are.conducted`))
-
-# Group by 'Funders' and count unique countries
-result2 <- PAHO_COVID_Projects %>%
-  group_by(Funders) %>%
-  summarise(Unique_Countries = n_distinct(`Country/.countries.research.is.being.are.conducted`))
